8ba407ec5694e4f2130f99f15d7a20889bcbcee7
Mauricio Muratori <maurimura@gmail.com>
2023-12-03 15:18:02 -0300

    WIP: need to check for overlaps

--- day-1/src/lib.rs
+++ day-1/src/lib.rs
@@ -20,7 +20,7 @@ fn digit_case_handler(char: char, digit_tuple: Option<(u32, u32)>) -> Option<(u3
     save_digit(number, digit_tuple)
 }
 
-const SPELLED_NUMBERS_BY_LENGTH: &'static [(&str, u32); 9] = &[
+const SPELLED_NUMBERS: &'static [(&str, u32); 9] = &[
     ("one", 1),
     ("two", 2),
     ("three", 3),
@@ -32,14 +32,19 @@ const SPELLED_NUMBERS_BY_LENGTH: &'static [(&str, u32); 9] = &[
     ("nine", 9),
 ];
 
-fn is_valid_char_at_position(char: char, position: usize) -> bool {
+fn is_valid_char_at_position(char: char, queue: &VecDeque<char>) -> bool {
     let mut valid = false;
-    for (spelled_number, _) in SPELLED_NUMBERS_BY_LENGTH {
+    let position = queue.len();
+    for (spelled_number, _) in SPELLED_NUMBERS {
         if spelled_number.len() <= position {
             continue;
         }
 
-        if spelled_number.chars().nth(position).unwrap() == char {
+        let spelled_number_current_char = spelled_number.chars().nth(position).unwrap();
+
+        if spelled_number_current_char == char
+            && spelled_number.starts_with(queue.iter().collect::<String>().as_str())
+        {
             valid = true;
             break;
         }
@@ -50,7 +55,7 @@ fn is_valid_char_at_position(char: char, position: usize) -> bool {
 fn process_queue(letters_queue: &VecDeque<char>) -> Option<u32> {
     let mut maybe_digit: Option<u32> = None;
 
-    for (spelled_number, number) in SPELLED_NUMBERS_BY_LENGTH {
+    for (spelled_number, number) in SPELLED_NUMBERS {
         if spelled_number.len() != letters_queue.len() {
             continue;
         }
@@ -72,16 +77,15 @@ fn process_queue(letters_queue: &VecDeque<char>) -> Option<u32> {
 }
 
 fn maybe_digit_from_queue(char: char, letters_queue: &mut VecDeque<char>) -> Option<u32> {
-    println!("char: {} - queue: {:?}", char, letters_queue);
-    if !is_valid_char_at_position(char, letters_queue.len()) {
-        if letters_queue.len() > 0 {
-            letters_queue.pop_front();
+    let queue_len = letters_queue.len();
+    if !is_valid_char_at_position(char, &letters_queue) {
+        if queue_len > 0 {
+            letters_queue.clear();
             return maybe_digit_from_queue(char, letters_queue);
         } else {
             return None;
         }
     }
-
     letters_queue.push_back(char);
     process_queue(letters_queue)
 }
@@ -99,7 +103,8 @@ fn spelled_case_handler(
     match maybe_digit {
         Some(digit) => {
             letters_queue.clear();
-            save_digit(digit, digit_tuple)
+            let digit_tuple = save_digit(digit, digit_tuple);
+            digit_tuple
         }
         None => digit_tuple,
     }
@@ -129,15 +134,9 @@ fn get_line_value_part_1(line: String) -> u32 {
 fn get_line_value_part_2(line: String) -> u32 {
     let mut digit_tuple: Option<(u32, u32)> = None;
     let mut letters_queue: VecDeque<char> = VecDeque::new();
-
     for char in line.chars() {
         if !char.is_alphanumeric() {
             letters_queue.clear();
-            continue;
-        }
-
-        if char.is_alphabetic() {
-            digit_tuple = spelled_case_handler(char, digit_tuple, &mut letters_queue);
             continue;
         }
 
@@ -146,8 +145,18 @@ fn get_line_value_part_2(line: String) -> u32 {
             digit_tuple = digit_case_handler(char, digit_tuple);
             continue;
         }
-    }
 
+        if char.is_alphabetic() {
+            digit_tuple = spelled_case_handler(char, digit_tuple, &mut letters_queue);
+            continue;
+        }
+    }
+    if line.chars().next().unwrap().is_alphabetic()
+        && line.chars().last().unwrap().is_alphabetic()
+        && line.len() > 32
+    {
+        println!("{} {:?}\n", line, digit_tuple)
+    }
     parse_tuple_valu(digit_tuple)
 }
 
@@ -222,5 +231,15 @@ mod tests {
         ];
         let total = day_1(input.into_iter().map(|s| Ok(s)), true);
         assert_eq!(total, 281)
+    }
+    #[test]
+    fn test_day_2_more_data() {
+        let input = vec![
+            "gsntbddbnone4cjqjmspzcsxmvvthreefive".to_string(),
+            "one7three9threeoneonetwo".to_string(),
+            "8pztdljxbjjthreenineeightseven7crkdr8eightwocb".to_string(),
+        ];
+        let total = day_1(input.into_iter().map(|s| Ok(s)), true);
+        assert_eq!(total, 15 + 12 + 88)
     }
 }
